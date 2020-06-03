5cfe6725bfb20e6f42c3747b40c079dcc6fcc58d
Tiger Nie <nhl0819@gmail.com>
2020-06-03 00:29:46 -0500

    added some unit tests, setup.py

--- setup.py
+++ setup.py
@@ -3,18 +3,30 @@ from setuptools import setup, find_packages
 with open("README.md") as readme_file:
     readme = readme_file.read()
 
-requirements = []
-
-setup_requirements = []
-
-test_requirements = []
+test_requirements = ["pytest>=3"]
 
 setup(
-    author="Tiger Nie"
+    author="Tiger Nie",
     author_email="nhl0819@gmail.com",
     python_requires=">=3.6",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-    ]
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    description="",
+    long_description=readme,
+    keywords="servio",
+    name="servio",
+    packages=find_packages(include=["servio", "servio.*"]),
+    test_requires=test_requirements,
+    url="https://github.com/tiega/servio",
+    version="0.0.1",
+    zip_safe=False,
 )
